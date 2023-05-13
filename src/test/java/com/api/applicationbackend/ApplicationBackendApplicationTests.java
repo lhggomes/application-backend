0111b15af27034a6c2b1afda5768e97b9dcde61d
Lucas Gomes <lucasgomes@Lucass-MacBook-Pro.local>
2023-05-13 11:41:51 -0300

    First Commit

---
+++ src/test/java/com/api/applicationbackend/ApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.applicationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
